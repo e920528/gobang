32b4e3b45832f325ecfbcbdb84b6e186a92e6279
YuWei Cheng <e920528@gmail.com>
2018-06-06 18:51:22 +0800

    判斷勝者功能完成

--- src/Game.js
+++ src/Game.js
@@ -15,7 +15,7 @@ class Game extends Component {
   handleClick(i) {
     if(this.state.squares[i] || this.state.finished) return;
     let squares = this.state.squares.slice();
-    squares[i] = this.state.blackIsNext ? 1 : 2;
+    squares[i] = this.state.blackIsNext ? 1 : 2;//black:1 white:2
     this.setState({
       lastMove: i,
       squares: squares
@@ -46,7 +46,103 @@ class Game extends Component {
 
   }
   determineWinner(lastMove){
-  
+    let row = Math.floor(lastMove/20);
+    let column = lastMove % 20;
+    let color = 1;
+    let connectNum = 0;
+    if(!this.state.blackIsNext)
+      color += 1;
+    let c = column - 1;
+    let r = row;
+    //水平
+    while (c >= 0) {
+      if(this.state.squares[20 * r + c] !== color)
+        break;
+      connectNum++;
+      if(connectNum >= 4)
+        return true;
+      c--;
+    }
+    c = column + 1;
+    while(c < 20){
+      if(this.state.squares[20 * r + c] !== color)
+        break;
+      connectNum++;
+      if(connectNum >= 4)
+        return true;
+      c++;
+    }
+    //鉛直
+    c = column;
+    r = row - 1;
+    connectNum = 0;
+    while(r >= 0){
+      if(this.state.squares[20 * r + c] !== color)
+        break;
+      connectNum++;
+      if(connectNum >= 4)
+        return true;
+      r--;
+    }
+    r = row + 1;
+    while(r < 20){
+      if(this.state.squares[20 * r + c] !== color)
+        break;
+      connectNum++;
+      if(connectNum >= 4)
+        return true;
+      r++;
+    }
+
+    //左上右下
+    c = column - 1;
+    r = row - 1;
+    connectNum = 0;
+    while(r >= 0 && c >= 0){
+      if(this.state.squares[20 * r + c] !== color)
+        break;
+      connectNum++;
+      if(connectNum >= 4)
+        return true;
+      r--;
+      c--;
+    }
+    r = row + 1;
+    c = column + 1;
+    while(r < 20 && c < 20){
+      if(this.state.squares[20 * r + c] !== color)
+        break;
+      connectNum++;
+      if(connectNum >= 4)
+        return true;
+      r++;
+      c++;
+    }
+    //右上左下
+    c = column + 1;
+    r = row - 1;
+    connectNum = 0;
+    while(r >= 0 && c < 20){
+      if(this.state.squares[20 * r + c] !== color)
+        break;
+      connectNum++;
+      if(connectNum >= 4)
+        return true;
+      r--;
+      c++;
+    }
+    r = row + 1;
+    c = column - 1;
+    while(r < 20 && c >= 0){
+      if(this.state.squares[20 * r + c] !== color)
+        break;
+      connectNum++;
+      if(connectNum >= 4)
+        return true;
+      r++;
+      c--;
+    }
+    return false;
   }
   render() {
     return (
