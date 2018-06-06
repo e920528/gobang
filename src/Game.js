54cc64e1d46a6f089790f3b558704099ecef6471
YuWei Cheng <e920528@gmail.com>
2018-06-06 19:18:39 +0800

    production

--- src/Game.js
+++ src/Game.js
@@ -9,7 +9,7 @@ class Game extends Component {
     squares: Array(400).fill(null),
     blackIsNext: true,
     lastMove: -1,
-    info: "",
+    info: "Next Player is: Black",
     finished: false
   }
   handleClick(i) {
@@ -38,7 +38,7 @@ class Game extends Component {
       else
         next = "Black";
       this.setState({
-        info: "Next Player is: " + next,
+        info: "Next Player is: Black",
         blackIsNext: !this.state.blackIsNext
       })
     }   
